c200f50ceb339b96e974ddc8cc9f77a4c1ed651c
Akash R Chandran <chandranrakash@gmail.com>
2022-09-03 10:44:44 +0530

    fixed all errors

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php
-require 'spotfy.php';
+require 'spotify.php';
 error_reporting(0);
+header("Content-Type: application/json");
 
+$trackid = $_GET['trackid'];
 
-
+if (! $trackid) {
+	http_response_code(400);
+	$reponse = json_encode(["error" => true, "message" => "trackid parameter is required!"]);
+	echo $reponse;
+	return;
+}
+$spotify = new Spotify();
+$spotify -> check_if_expire();
+$reponse = $spotify -> get_lyrics(track_id: $trackid);
 ?>
--- spotify.php
+++ spotify.php
@@ -2,16 +2,11 @@
 
 class Spotify
 {
-	private $sp_dc;
 	private $spotify_url = 'https://open.spotify.com/';
-	function __construct($sp_dc)
+	private $lyrics_url = 'https://spclient.wg.spotify.com/color-lyrics/v2/track/';
+	function get_token()
 	{
-		$this->sp_dc = $sp_dc;
-	}
-
-	function get_token($sp_dc)
-	{
-		echo $sp_dc;
+		$sp_dc = getenv('SP_DC');
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_TIMEOUT, 600);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
@@ -19,23 +14,51 @@ class Spotify
 		curl_setopt($ch, CURLOPT_VERBOSE, 0);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_HEADER, 1);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
 		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
 		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
 			"User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Safari/537.36",
-			"app-platform: WebPlayer", 
-			"Cookie: sp_dc = $sp_dc"
+			"App-platform: WebPlayer", 
+			"content-type: text/html; charset=utf-8",
+			"cookie: sp_dc=$sp_dc;"
 		));
-		curl_setopt($ch, CURLOPT_URL, 'https://open.spotify.com/');
+		curl_setopt($ch, CURLOPT_URL, $this -> spotify_url);
 		$result = curl_exec($ch);
-		$re = '~<script id="config" data-testid="config" type="application/json">(\S+)</script>~m';
+		$re = '~<script id="session" data-testid="session" type="application/json">(\S+)</script>~m';
 		preg_match_all($re, $result, $matches, PREG_SET_ORDER, 0);
 		$token_json = $matches[0][1];
 		$token_file = fopen("config.json", "w") or die("Unable to open file!");;
 		fwrite($token_file, $token_json);
-		echo $token_json;
+	}
+
+	function check_if_expire()
+	{
+		$check = file_exists("config.json");
+		if ($check) {
+			$json = file_get_contents("config.json");
+			$timeleft = json_decode($json,true)['accessTokenExpirationTimestampMs'];
+			$timenow = round(microtime(true) * 1000);
+		}
+		if (!$check || $timeleft < $timenow) {
+			$this -> get_token();
+		}
+	}
+
+	function get_lyrics($track_id)
+	{	
+		$json = file_get_contents('config.json');
+		$token = json_decode($json,true)['accessToken'];
+		$formated_url = $this -> lyrics_url.$track_id.'?format=json&market=from_token';
+
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+			"User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Safari/537.36",
+			"App-platform: WebPlayer",
+			"authorization: Bearer $token"
+		));
+		curl_setopt($ch, CURLOPT_URL, $formated_url);
+		$result = curl_exec($ch);
+		return $result;
 	}
 }
-
-$new = new Spotify('trial');
-$new->get_token('trial');
