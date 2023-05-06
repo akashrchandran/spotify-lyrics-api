bee9c5c431cc40795d5932833688dcfb50d61678
Akash R Chandran <chandranrakash@gmail.com>
2023-05-06 13:48:34 +0530

    updated

--- index.php
+++ index.php
@@ -19,23 +19,20 @@ if ($url) {
 	$trackid = $matches[1][0];
 }
 $spotify = new Spotify();
-$spotify->check_if_expire();
-$reponse = $spotify->get_lyrics(track_id: $trackid);
+$spotify->checkTokenExpire();
+$reponse = $spotify->getLyrics(track_id: $trackid);
 echo make_reponse($reponse, $typed);
 
 function make_reponse($response, $format)
 {
+	global $spotify;
 	$temp = json_decode($response, true)['lyrics'];
 	if (!$temp) {
 		http_response_code(404);
 		return json_encode(["error" => true, "message" => "lyrics for this track is not available on spotify!"]);
 	}
 	if ($format == 'lrc') {
-		$lines = array();
-		foreach ($temp['lines'] as $lists) {
-			$lrctime = formatMS($lists['startTimeMs']);
-			array_push($lines, ["timeTag" => $lrctime, "words" => $lists['words']]);
-		}
+		$lines = $spotify -> getLrcLyrics($temp["lines"]);
 		$response = ["error" => false, "syncType" => $temp["syncType"], "lines" => $lines];
 	} else {
 		$response = ["error" => false, "syncType" => $temp["syncType"], "lines" => $temp["lines"]];
@@ -43,17 +40,4 @@ function make_reponse($response, $format)
 	return json_encode($response);
 }
 
-function formatMS($milliseconds)
-{
-	if ($milliseconds == 0) {
-		return '00:00.00';
-	}
-	$seconds = floor($milliseconds / 1000);
-	$minutes = floor($seconds / 60);
-	$centi = $milliseconds % 1000;
-	$seconds = $seconds % 60;
-	$minutes = $minutes % 60;
-	$format = '%02u:%02u.%03u';
-	$time = sprintf($format, $minutes, $seconds, $centi);
-	return rtrim($time, '0');
-}
+
