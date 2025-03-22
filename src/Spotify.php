eef745b63cfecc0ac8f04c14a0db949ec4a89c49
Akash R Chandran <chandranrakash@gmail.com>
2025-03-22 20:37:56 +0530

    Fix: Add 'App-platform' header for WebPlayer in Spotify.php

--- src/Spotify.php
+++ src/Spotify.php
@@ -278,6 +278,7 @@ class Spotify {
             $headers = [
                 'accept: application/json',
                 'content-type: application/json',
+                'App-platform: WebPlayer',
                 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Safari/537.36'
             ];
 
